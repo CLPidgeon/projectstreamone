8f58eacfeaaf4d611f8f35dce3959b9d03620d3b
Clare <clarelpidgeon@yahoo.co.uk>
2017-10-03 16:14:53 +0100

    unittesting the game

--- project/javascript_testing/spec/guessingSpec.js
+++ project/javascript_testing/spec/guessingSpec.js
@@ -1,18 +1,27 @@
 describe("birdGame", function() {
 
-	describe("If answer is correct", function() {
-		it("should have called the alert function saying CORRECT", function() {
-			spyOn(window, "alert");
-			guessBird("PUFFIN");
-			expect(window.alert).toHaveBeenCalledWith("CORRECT! This is a PUFFIN");
+	describe("If answer is correct in lowercase", function() {
+		it("should have called the alert function saying 'correct' ", function() {
+			expect(checkBird("puffin")).toBe(true);
+		});
+	});
+
+	describe("If answer is correct in uppercase", function() {
+		it("should have called the alert function saying 'correct' ", function() {
+			expect(checkBird("PUFFIN")).toBe(true);
+		});
+	});
+
+	describe("If answer is correct in mixed cases", function() {
+		it("should have called the alert function saying 'correct' ", function() {
+			expect(checkBird("PuffIN")).toBe(true);
 		});
 	});
 
 	describe("If answer is incorrect", function() {
-		it("should have called the alert function if answer is wrong", function() {
-			spyOn(window, "alert");
-			guessBird();
-			expect(window.alert).toHaveBeenCalledWith("INCORRECT! Try again.");
+		it("should have called the alert function saying 'incorrect' ", function() {
+			expect(checkBird("puffing")).toBe(false);
 		});
 	});
+
 });
