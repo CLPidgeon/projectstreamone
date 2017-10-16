08734f7fddb360d09fe4fd7adea6e0697b80c24d
Clare <clarelpidgeon@yahoo.co.uk>
2017-10-16 15:15:25 +0100

    code tidy up

--- project/js/guessing.js
+++ project/js/guessing.js
@@ -1,21 +1,15 @@
 function checkBird(guess) {
-
 	return guess.toUpperCase() === "PUFFIN";
 }
 
 function guessBird(){
-
 	var guess = document.getElementById("userGuess").value;	
-
 	if (checkBird(guess)) {
-		alert("CORRECT! This is a Puffin");
-		
+		alert("CORRECT! This is a Puffin");		
 	} else {
 		alert("INCORRECT! Try again.");
 	};
-
 	document.getElementById("userGuess").value ="";
-
 };
 
 function clearBox() {
